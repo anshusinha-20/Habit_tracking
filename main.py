e55d46aaf7260738445e688c437cf6ef40001bc0
Anshu Sinha <anshujuly2@gmail.com>
2023-04-15 20:19:26 +0530

    Add a code for advanced authentication

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 """imported requests module"""
 import requests
 
+"""contants"""
+USERNAME = "anshusinha20"
+TOKEN = "ds2d233kjh44jwkjb23b3kjd2"
+
 """variable to store endpoint"""
 pixelaEndpoint = "https://pixe.la/v1/users"
 
 """dictionary to hold the parameters"""
 pixelaParameters = {
-    "token": "ds2d233kjh44jwkjb23b3kjd2",
-    "username": "anshusinha20",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-"""posting the request"""
-response = requests.post(url=pixelaEndpoint, json=pixelaParameters)
+# """posting the request"""
+# response = requests.post(url=pixelaEndpoint, json=pixelaParameters)
+# print(response.text)
+
+"""variable to store the graph endpoint"""
+graphEndpoint = f"{pixelaEndpoint}/{USERNAME}/graphs"
+
+"""dictionary to hold the graph parameters"""
+graphParameters = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graphEndpoint, json=graphParameters, headers=headers)
 print(response.text)
